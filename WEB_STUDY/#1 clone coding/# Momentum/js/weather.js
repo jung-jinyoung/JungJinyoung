ec70f57fd6993f5526ae25bd9abc520515fb9f5e
jinyoung <155875885+jung-jinyoung@users.noreply.github.com>
2024-06-08 00:39:20 +0900

    delete API_KEY
    delete weather OPEN API KEY

--- WEB_STUDY/#1 clone coding/# Momentum/js/weather.js
+++ WEB_STUDY/#1 clone coding/# Momentum/js/weather.js
@@ -2,7 +2,7 @@ function onGeoOk(position){
     // 현재 위도와 경도 불러오기 
     const lat = position.coords.latitude
     const lon = position.coords.longitude
-    const API_Key = '188ebc00b2781481d8232ca896dc564e'
+    const API_Key = ''
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_Key}`
     console.log(url)
     
